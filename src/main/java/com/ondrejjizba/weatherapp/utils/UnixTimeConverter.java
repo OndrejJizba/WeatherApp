0e9a3fbbc09d457ec1089c67aea53089d4c2664e
Ondrej Jizba <ondrej.jizba@gmail.com>
2024-05-23 13:34:38 +0200

    Fixed UnixTimeConverter to work correctly when timezone is less than UTC+0 and refactored the class.

--- src/main/java/com/ondrejjizba/weatherapp/utils/UnixTimeConverter.java
+++ src/main/java/com/ondrejjizba/weatherapp/utils/UnixTimeConverter.java
@@ -7,14 +7,23 @@ import java.time.format.DateTimeFormatter;
 
 public class UnixTimeConverter {
     public static String converterTime(long unixTime, long timezone) {
-        Instant instant = Instant.ofEpochSecond(unixTime);
-        ZonedDateTime zonedDateTime = instant.atZone(ZoneId.of("UTC+" + timezone/3600));
+        ZonedDateTime zonedDateTime = convertToLocalDateTime(unixTime, timezone);
         return zonedDateTime.format(DateTimeFormatter.ofPattern("HH:mm:ss"));
     }
 
     public static String converterDayTime(long unixTime, long timezone) {
-        Instant instant = Instant.ofEpochSecond(unixTime);
-        ZonedDateTime zonedDateTime = instant.atZone(ZoneId.of("UTC+" + timezone/3600));
+        ZonedDateTime zonedDateTime = convertToLocalDateTime(unixTime, timezone);
         return zonedDateTime.format(DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm:ss"));
+    }
+
+    private static ZonedDateTime convertToLocalDateTime(long unixTime, long timezone) {
+        Instant instant = Instant.ofEpochSecond(unixTime);
+        ZonedDateTime zonedDateTime;
+        if (timezone < 0) {
+            zonedDateTime = instant.atZone(ZoneId.of("UTC" + timezone / 3600));
+        } else {
+            zonedDateTime = instant.atZone(ZoneId.of("UTC+" + timezone / 3600));
+        }
+        return zonedDateTime;
     }
 }
