637015ade09c68cffbf52af5fe33347527e02ddf
Ondrej Jizba <ondrej.jizba@gmail.com>
2024-05-24 12:44:12 +0200

    Added Router and Bootstrap.

--- src/main/ui/src/App.js
+++ src/main/ui/src/App.js
@@ -1,13 +1,28 @@
-import React from "react";
-import Weather from "./components/Weather";
+import React, { useEffect, useState } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import axios from "axios";
 import WeatherList from "./components/WeatherList";
+import DetailedWeather from "./components/DetailedWeather";
 
 const App = () => {
+  const [weatherData, setWeatherData] = useState([]);
+
+  useEffect(() => {
+    const fetchWeatherData = async () => {
+      const response = await axios.get("/cities");
+      setWeatherData(response.data);
+    };
+
+    fetchWeatherData();
+  }, []);
+
   return (
-    <div className="App">
-      {/* <Weather cityId={2} /> */}
-      <WeatherList />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<WeatherList />} />
+        <Route path="/weather/:id" element={<DetailedWeather />} />
+      </Routes>
+    </Router>
   );
 };
 
