e178a0086e434b5b9be9f2f4d92e0c519c221ba3
Ondrej Jizba <ondrej.jizba@gmail.com>
2024-06-06 16:10:10 +0200

    JWT authentication mechanism has been implemented. It involves the addition of JwtRequestFilter and JwtTokenUtil classes, which handle the extraction and validation of JWT tokens.UserDetailsServiceImp has also been added to fetch user details for the authentication process.

---
+++ src/main/java/com/ondrejjizba/weatherapp/services/UserDetailsServiceImp.java
@@ -0,0 +1,31 @@
+package com.ondrejjizba.weatherapp.services;
+
+import com.ondrejjizba.weatherapp.models.UserInfo;
+import com.ondrejjizba.weatherapp.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+
+@Service
+public class UserDetailsServiceImp implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserDetailsServiceImp(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        UserInfo user = userRepository.findByUsername(username);
+        if (user == null) {
+            throw new UsernameNotFoundException("user not found with username: " + username);
+        }
+        return new User(user.getUsername(), user.getPassword(), new ArrayList<>());
+    }
+}
---
+++ src/main/java/com/ondrejjizba/weatherapp/utils/JwtRequestFilter.java
@@ -0,0 +1,52 @@
+package com.ondrejjizba.weatherapp.utils;
+
+import com.ondrejjizba.weatherapp.services.UserDetailsServiceImp;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class JwtRequestFilter extends OncePerRequestFilter {
+    private final UserDetailsServiceImp userDetailsServiceImp;
+    private final JwtTokenUtil jwtTokenUtil;
+
+    public JwtRequestFilter(UserDetailsServiceImp userDetailsServiceImp, JwtTokenUtil jwtTokenUtil) {
+        this.userDetailsServiceImp = userDetailsServiceImp;
+        this.jwtTokenUtil = jwtTokenUtil;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        final String authorizationHeader = request.getHeader("Authorization");
+
+        String username = null;
+        String jwt = null;
+
+        if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
+            jwt = authorizationHeader.substring(7);
+            username = jwtTokenUtil.extractUsername(jwt);
+        }
+
+        if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+            UserDetails userDetails = this.userDetailsServiceImp.loadUserByUsername(username);
+
+            if (jwtTokenUtil.validateToken(jwt, userDetails)) {
+                UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(
+                        userDetails, null, userDetails.getAuthorities());
+                usernamePasswordAuthenticationToken.setDetails(
+                        new WebAuthenticationDetailsSource().buildDetails(request));
+                SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
+            }
+        }
+        filterChain.doFilter(request, response);
+    }
+}
