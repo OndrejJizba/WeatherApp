4590277917fbd34ce3a4b3b63ec30698c97f37c3
Ondrej Jizba <ondrej.jizba@gmail.com>
2024-06-06 16:13:03 +0200

    Added JWT authentication to the application. Implemented a new JwtRequestFilter and updated the SecurityConfig to use it. Also updated UserController to handle new authenticate route where the token generation occurs.

--- src/main/java/com/ondrejjizba/weatherapp/configuration/SecurityConfig.java
+++ src/main/java/com/ondrejjizba/weatherapp/configuration/SecurityConfig.java
@@ -1,29 +1,47 @@
 package com.ondrejjizba.weatherapp.configuration;
 
+import com.ondrejjizba.weatherapp.utils.JwtRequestFilter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+    private final JwtRequestFilter jwtRequestFilter;
+    @Autowired
+    public SecurityConfig(JwtRequestFilter jwtRequestFilter) {
+        this.jwtRequestFilter = jwtRequestFilter;
+    }
 
     @Bean
-    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers("/weather", "/search", "/cities", "/forecast/**", "/images/**").permitAll()
-                        .anyRequest().permitAll())
+                        .requestMatchers("/weather", "/search", "/cities", "/forecast/**", "/images/**", "/registration", "/authenticate").permitAll()
+                        .anyRequest().authenticated())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .csrf(AbstractHttpConfigurer::disable);
+        http.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
 
     @Bean
     public static PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
     }
 }
--- src/main/java/com/ondrejjizba/weatherapp/controllers/UserController.java
+++ src/main/java/com/ondrejjizba/weatherapp/controllers/UserController.java
@@ -1,13 +1,21 @@
 package com.ondrejjizba.weatherapp.controllers;
 
 import com.ondrejjizba.weatherapp.exceptions.ValidationError;
-import com.ondrejjizba.weatherapp.models.DTOs.RegistrationRequest;
+import com.ondrejjizba.weatherapp.models.DTOs.UsernamePasswordRequest;
 import com.ondrejjizba.weatherapp.repositories.UserRepository;
+import com.ondrejjizba.weatherapp.services.UserDetailsServiceImp;
 import com.ondrejjizba.weatherapp.services.UserService;
+import com.ondrejjizba.weatherapp.utils.JwtTokenUtil;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.DisabledException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,16 +27,22 @@ import java.util.Map;
 public class UserController {
     private final UserService userService;
     private final UserRepository userRepository;
+    private final AuthenticationManager authenticationManager;
+    private final JwtTokenUtil jwtTokenUtil;
+    private final UserDetailsServiceImp userDetailsServiceImp;
 
     @Autowired
     public UserController(UserService userService,
-                          UserRepository userRepository) {
+                          UserRepository userRepository, AuthenticationManager authenticationManager, JwtTokenUtil jwtTokenUtil, UserDetailsServiceImp userDetailsServiceImp) {
         this.userService = userService;
         this.userRepository = userRepository;
+        this.authenticationManager = authenticationManager;
+        this.jwtTokenUtil = jwtTokenUtil;
+        this.userDetailsServiceImp = userDetailsServiceImp;
     }
 
     @PostMapping("/registration")
-    public ResponseEntity<?> registration(@Valid @RequestBody RegistrationRequest request,
+    public ResponseEntity<?> registration(@Valid @RequestBody UsernamePasswordRequest request,
                                           BindingResult bindingResult) {
         if (bindingResult.hasErrors()) {
             return ValidationError.handleErrors(bindingResult);
@@ -39,5 +53,31 @@ public class UserController {
             return ResponseEntity.status(400).body(result);
         }
         return ResponseEntity.status(200).body(userService.userRegistrationSuccessful(request));
+    }
+
+    @PostMapping("/authenticate")
+    public ResponseEntity<?> createAuthenticationToken(@RequestBody UsernamePasswordRequest request) throws Exception {
+        authenticate(request.getUsername(), request.getPassword());
+
+        UserDetails userDetails = userDetailsServiceImp.loadUserByUsername(request.getUsername());
+        String token = jwtTokenUtil.generateToken(userDetails);
+        Map<String, String> result = new HashMap<>();
+        result.put("jwtToken", token);
+        return ResponseEntity.status(200).body(result);
+    }
+
+    private void authenticate(String username, String password) throws Exception {
+        try {
+            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username, password));
+        } catch (DisabledException e) {
+            throw new Exception("USER_DISABLED", e);
+        } catch (BadCredentialsException e) {
+            throw new Exception("INVALID_CREDENTIALS", e);
+        }
+    }
+
+    @GetMapping("/test")
+    public String test(){
+        return "Hello";
     }
 }
